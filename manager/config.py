728c0554ef54b1afc3a505f02e3632057c53498f
Vishnu <vishnunilambur@gmail.com>
2019-09-18 23:01:39 +0530

    modified config

--- manager/config.py
+++ manager/config.py
@@ -5,5 +5,5 @@ class Config:
     MAIL_SERVER = 'smtp.sendgrid.net'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'apikey'
-    MAIL_PASSWORD = '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'
+    MAIL_USERNAME = 'some username'
+    MAIL_PASSWORD = 'some password'
